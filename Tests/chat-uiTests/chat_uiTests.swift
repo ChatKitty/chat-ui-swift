824826531a4e4204a505037a87327f79c278e678
Aaron Nwabuoku <aaron@chatkitty.com>
2023-11-03 16:29:38 -0400

    Initial Commit

---
+++ Tests/chat-uiTests/chat_uiTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import chat_ui
+
+final class chat_uiTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
