f5215e586fc54d60a01f9b390c27442ea62b39ec
Alex Nguyen <itsAlexNguyen@users.noreply.github.com>
2023-11-22 18:19:48 -0500

    feat(chat-ui): Commit code generated models
    Created code generated models that do not get included into the build.

---
+++ ChatKittyUI/Classes/models/Notification.swift
@@ -0,0 +1,10 @@
+struct BaseNotification: Codable {
+    let id: Int
+    let title: String
+    let body: String
+//    let channel: Channel?
+    var data: AnyCodable
+    let muted: Bool
+    let createdTime: String
+    var readTime: String?
+}
